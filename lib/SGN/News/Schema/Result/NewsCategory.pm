33a2abe269f947607c70c815b865880e297ab701
Robert Buels <rmb32@cornell.edu>
2010-05-25 16:16:07 -0700

    added many-to-many for categories -> stories

--- lib/SGN/News/Schema/Result/NewsCategory.pm
+++ lib/SGN/News/Schema/Result/NewsCategory.pm
@@ -76,6 +76,10 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07000 @ 2010-05-24 12:30:18
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:OnUPvGMPmnlHvVphTfJZVQ
 
+__PACKAGE__->many_to_many(
+    'news_stories',
+    'news_story_categories' => 'news_story',
+   );
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
